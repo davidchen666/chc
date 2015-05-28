fe66fd4efb7c08265cbd158fefa4a430f35edacb
david <david@d73c5ef6-8d2e-41fd-9d27-8543725b6265>
2015-05-28 03:14:09 +0000

    git-svn-id: http://203.156.255.138/svn/iresearch_ui@830 d73c5ef6-8d2e-41fd-9d27-8543725b6265

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $file_name = basename(__FILE__,'.php');
 $controller = $_request->get('m',$file_name);
 
 $action = $_request->get('a','index');
-//验证是否登录
+//验证是否登录1
 //	$controller != 'login' && Controller::instance('login')->isLogin();
 Controller::instance($controller)->{$action}();
 ?>
