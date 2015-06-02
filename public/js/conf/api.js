2d84fa60120dd69f8d7ea956eba6506f7c561816
david <david@d73c5ef6-8d2e-41fd-9d27-8543725b6265>
2015-06-02 02:56:39 +0000

    git-svn-id: http://203.156.255.138/svn/iresearch_ui@842 d73c5ef6-8d2e-41fd-9d27-8543725b6265

--- public/js/conf/api.js
+++ public/js/conf/api.js
@@ -3,5 +3,6 @@ define({
     user: "test/api/user",
     succeed: "test/api/succeed",
     tableList:"iresearch_ui/test/tablelist.json",
-    getExcelData:"http://10.10.21.163/iadm_web/?m=Excel&a=exportPR"
+    getExcelData:"http://10.10.21.163/iadm_web/?m=Excel&a=exportPR",
+    treeList:"iresearch_ui/test/jsTree.json"
 });
