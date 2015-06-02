3a74fe6aeeb07a680af2aaa5804db7c8a7a28399
hyanwang <hyanwang@d73c5ef6-8d2e-41fd-9d27-8543725b6265>
2015-06-02 03:33:34 +0000

    add datelist
    git-svn-id: http://203.156.255.138/svn/iresearch_ui@850 d73c5ef6-8d2e-41fd-9d27-8543725b6265

--- public/js/conf/api.js
+++ public/js/conf/api.js
@@ -4,5 +4,6 @@ define({
     succeed: "test/api/succeed",
     tableList:"iresearch_ui/test/tablelist.json",
     getExcelData:"http://10.10.21.163/iadm_web/?m=Excel&a=exportPR",
-    treeList:"iresearch_ui/test/jsTree.json"
+    treeList:"iresearch_ui/test/jsTree.json",
+    datelist: "iresearch_ui/test/datelist.json"
 });
