8f174982b556afe48abf71f2fa1a42f6e82d7cc7
david <david@d73c5ef6-8d2e-41fd-9d27-8543725b6265>
2015-06-02 06:36:46 +0000

    git-svn-id: http://203.156.255.138/svn/iresearch_ui@851 d73c5ef6-8d2e-41fd-9d27-8543725b6265

--- public/js/modules/datepicker/app.js
+++ public/js/modules/datepicker/app.js
@@ -11,7 +11,7 @@ define(function(require, exports, module) {
             textForamt: "YYYY年MM月DD日",
             defaultDate: [moment().subtract('days', 6), moment()], //默认最近一周
         }, function(start, end) {
-            $p.plugin.alert("开始时间：" + start + " , 结束时间：" + end);
+            $p.com.alert("开始时间：" + start + " , 结束时间：" + end);
         });
 
     };
@@ -22,7 +22,7 @@ define(function(require, exports, module) {
             textFormat: "YYYY年MM月DD日",
             defaultDate: moment(),
         }, function(date) {
-            $p.plugin.alert(date);
+            $p.com.alert(date);
         });
     };
 
