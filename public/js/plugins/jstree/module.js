212e2a878720f5d456efdd4a9a258715b701b262
david <david@d73c5ef6-8d2e-41fd-9d27-8543725b6265>
2015-06-02 02:56:55 +0000

    git-svn-id: http://203.156.255.138/svn/iresearch_ui@843 d73c5ef6-8d2e-41fd-9d27-8543725b6265

--- public/js/plugins/jstree/module.js
+++ public/js/plugins/jstree/module.js
@@ -10,26 +10,42 @@ define(function(require, exports, module) {
     require('./dist/themes/default/style.min.css');
     var g = window;
     function irsJsTree(seletor, options){
-        seletor.on("changed.jstree", function (e, data) {
-            if(data.selected.length) {
-                alert('点击: ' + data.instance.get_node(data.selected[0]).text);
-            }
-        })
-            .jstree({
-                'core' : {
-                    'data' : [
-                        { "text" : "媒体", "children" : [
-                            { "text" : "新浪","id":89, "children" : [
-                                { "text" : "房产频道","id":1189 },
-                                { "text" : "汽车频道","id":1187 }
-                            ]},
-                            { "text" : "网易","id":90 }
-                        ]}
-                    ]
-                },
-                "plugins" : [ "checkbox"]
-            });
+        getDataList(seletor, options);
+//        seletor.on("changed.jstree", function (e, data) {
+//            if(data.selected.length) {
+//                //alert('点击: ' + data.instance.get_node(data.selected[0]).text);
+//            }
+//        })
+//            .jstree({
+//                'core' : {
+//                    'data' : [
+//                        { "text" : "媒体", "children" : [
+//                            { "text" : "新浪","id":89, "children" : [
+//                                { "text" : "房产频道","id":1189 },
+//                                { "text" : "汽车频道","id":1187 }
+//                            ]},
+//                            { "text" : "网易","id":90 }
+//                        ]}
+//                    ]
+//                },
+//                "plugins" : [ "checkbox"]
+//            });
 
+    }
+    function getDataList(seletor, options){
+        options.dataSource(options,function(dataSource){
+            seletor.on("changed.jstree", function (e, data) {
+                if(data.selected.length) {
+                    //alert('点击: ' + data.instance.get_node(data.selected[0]).text);
+                }
+            })
+                .jstree({
+                    'core' : {
+                        'data' : dataSource
+                    },
+                    "plugins" : options.plugins
+                });
+        });
     }
     g[PagurianAlias].plugin.irsTree = function(seletor, options) {
         var table = new irsJsTree(seletor, options);
