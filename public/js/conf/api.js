cea3471f01cdd0eaf8be5ad6df482ab6e89ed9d9
david <david@d73c5ef6-8d2e-41fd-9d27-8543725b6265>
2015-06-04 09:14:43 +0000

    git-svn-id: http://203.156.255.138/svn/iresearch_ui@879 d73c5ef6-8d2e-41fd-9d27-8543725b6265

--- public/js/conf/api.js
+++ public/js/conf/api.js
@@ -1,7 +1,7 @@
 define({
-    keywords: "uiframe/test/api/keywords",
-    user: "test/api/user",
-    succeed: "test/api/succeed",
+    keywords: "iresearch_ui/test/keywords",
+    user: "iresearch_ui/test/user",
+    succeed: "iresearch_ui/test/succeed",
     tableList:"iresearch_ui/test/tablelist.json",
     getExcelData:"http://10.10.21.163/iadm_web/?m=Excel&a=exportPR",
     treeList:"iresearch_ui/test/jsTree.json",
