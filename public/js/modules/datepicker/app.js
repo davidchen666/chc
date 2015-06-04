902e28124e90f8ecdf4af56d0f125430a5422b31
hyanwang <hyanwang@d73c5ef6-8d2e-41fd-9d27-8543725b6265>
2015-06-04 09:05:40 +0000

    update datepicker
    git-svn-id: http://203.156.255.138/svn/iresearch_ui@876 d73c5ef6-8d2e-41fd-9d27-8543725b6265

--- public/js/modules/datepicker/app.js
+++ public/js/modules/datepicker/app.js
@@ -1,31 +1,40 @@
 define(function(require, exports, module) {
 
     var app = require('../../lib/app');
-    require('../../plugins/daterangepicker/module');
 
+    require('../../plugins/daterangepicker/module');
     require('../../plugins/datepicker/module');
 
     app.page.daterange = function() {
-
         $p.plugin.dateRangePicker("#my_picker", {
             textForamt: "YYYY年MM月DD日",
             defaultDate: [moment().subtract('days', 6), moment()], //默认最近一周
         }, function(start, end) {
             $p.com.alert("开始时间：" + start + " , 结束时间：" + end);
         });
-
     };
 
     app.page.date = function() {
-
         $p.plugin.datePicker('#my_picker', {
+            format: 'YYYY-MM-DD',
             textFormat: "YYYY年MM月DD日",
+            startView:0,
             defaultDate: moment(),
         }, function(date) {
-            $p.com.alert(date);
+            $p.com.alert(date.format_date);
+            console.log(date);
+        });
+        $p.plugin.datePicker('#single_montn', {
+            format:"YYYY-MM-DD",
+            textFormat: "YYYY年MM月",
+            startView:0,
+            minViewMode:'months',
+            defaultDate: moment(),
+        }, function(date) {
+            $p.com.alert(date.format_date);
+            console.log(date);
         });
     };
-
 
     module.exports = app;
 
