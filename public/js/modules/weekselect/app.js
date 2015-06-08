6696db90a94b63a14548e03225192ee59d01d4c6
hyanwang <hyanwang@d73c5ef6-8d2e-41fd-9d27-8543725b6265>
2015-06-08 05:14:23 +0000

    mutilchoice
    git-svn-id: http://203.156.255.138/svn/iresearch_ui@882 d73c5ef6-8d2e-41fd-9d27-8543725b6265

--- public/js/modules/weekselect/app.js
+++ public/js/modules/weekselect/app.js
@@ -8,12 +8,28 @@ define(function (require, exports, module) {
     require('../../plugins/weekselect/module');
     app.page.weekselect = function () {
         $p.plugin.weekselect("#select_week", {
-            yearlist: model.getYearList,
-            multichoice: false
+            yearlist: model.getYearList, //年份限制
+            multichoice: true, //是否多选
+            limit_mulitichoice: 2, //多选限制
+            merge_data: true //是否日期对象数组，编程为
         }, function (data) {
-            //console.log(data);
-            $("#select_week").val(data.start_day + "~" + data.end_day);
+
+            if (Array.isArray(data)) {
+                if (data.merge_data()) {
+                    console.log(data);
+                    $("#select_week").val(data.start_day + "~" + data.end_day);
+                } else {
+                    //对于不整合日期对象数组的时候，可以在下面进行操作。
+                    data.forEach(function (ele, index) {
+                        console.log("序列号：" + index + ", 开始日期:" + ele.start_day + ", 结束日期:" + ele.end_day);
+                    });
+                }
+            } else {
+                $("#select_week").val(data.start_day + "~" + data.end_day);
+            }
+
         });
+
     };
     module.exports = app;
 });
