e122d827812295f23d3f7cb31fec799aed8136d9
hyanwang <hyanwang@d73c5ef6-8d2e-41fd-9d27-8543725b6265>
2015-09-21 06:02:06 +0000

    git-svn-id: http://203.156.255.138/svn/iresearch_ui@2021 d73c5ef6-8d2e-41fd-9d27-8543725b6265

--- public/js/lib/com/twig.js
+++ public/js/lib/com/twig.js
@@ -6,34 +6,86 @@
  * twig.js
  * copy parent node and append to next node
  */
+//exp:
+//        $p.com.twig("#user_attr", {
+  //twig: '.twig',
+  //    plus: ".userattr_plus",
+  //    minus: ".userattr_minus",
+  //    limit_num: 2,
+  //    init: function () {
+  //},
+  //complete: function (tag_id) {
+  //  $('#' + tag_id + '>.col-md-4>.form-group>#userattr_condition').attr('disabled', 'disable');
+  //  $('#' + tag_id + '>.col-md-4>.form-group>#userattr_where').attr('disabled', 'disable');
+  //  // console.log($(this)[0].body);
+  //  if (typeof opt === 'function') {
+  //    opt();
+  //  } else {
+  //    console.log(opt);
+  //  }
+  //}
+  //});
 
-define(function(require, exports, module) {
+define(function (require, exports, module) {
   var g = window;
 
   function Twig() {
-    this.init = function(selector, options) {
+    this.init = function (selector, options) {
+      //var twig_id = '_' + (Math.random() * 1E18).toString(36).slice(0, 5).toUpperCase();
       var clone_exmplate = $(selector + '>' + options.twig);
-      var item_html = clone_exmplate.outerHTML();
+      var i = 0; //id计数
+      var id_tag = selector.substring(1, selector.length); //删除头一个字符作为id的标签
+      clone_exmplate.attr('id', id_tag + i);
+      if (typeof options.init === 'function') {
+        options.init();
+      }
+      if ($(selector + '>' + options.twig) === 1) {
+        $(selector + '>' + options.twig + '>' + options.minus).addClass('disable');
+      }
 
-      $(document).delegate(options.plus, 'click', function() {
-        $(selector).append(item_html);
-      });
+      i = $(selector).children(options.twig).length;
+      // plus stuff
+      $(document).delegate(options.plus, 'click', function () {
 
-      $(document).delegate(options.minus, 'click', function() {
-        if ($(selector + '>' + options.twig).length === 1) {
+        $(this).parent(options.twig).attr('co_twig', i);
+        i++;
 
+        //限定条数
+        // if(parseInt(options.limit_num) == (parseInt(i)+1)){
+        //   console.log(options.plus);
+        //   $(options.plus).attr('disabled','disable');
+        // }
+
+        if (typeof options.body != 'undefined' || options.body != '') {
+          var tmp = clone_exmplate.clone();
         } else {
+          var tmp = options.body;
+        }
+        var item_html = tmp.attr('id', id_tag + i).outerHTML();
+        $(selector).append(item_html);
+        if (typeof options.complete === 'function') {
+          options.complete(id_tag + i);
+        }
+      });
+      // minus stuff
+      $(document).delegate(options.minus, 'click', function () {
+        //只剩一下一条的时候不做减少操作
+        if ($(selector + '>' + options.twig).length === 1) {
+          console.log('只有一条了');
+          // $(this).attr({disable:'disable'});
+          $(this).addClass('disable');
+        } else {
+          console.log($(selector + '>' + options.twig).length);
           $(this).parents(options.twig).remove();
-          console.log($(this).parents().find(options.twig));
+          // console.log($(this).parents().find(options.twig));
         }
       });
     }
+  }
 
-  };
-  g[PagurianAlias].com.twig = function(selector, options) {
+  g[PagurianAlias].com.twig = function (selector, options) {
     var twig = new Twig();
     twig.init(selector, options);
     return twig;
   }
-
 });
