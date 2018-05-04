8617bc7b49f4dd7418341b3725a38084716bc594
[david <[dawei.chen@meta-insight.com]>
2018-05-04 18:24:07 +0800

    获取会议model层

--- model/model.events.php
+++ model/model.events.php
@@ -5,7 +5,13 @@ class EventsModel extends AgentModel
     public function addMSignUp($data){
     	return curl_request(API_PATH.'m=events&a=addMSignUp',$data);
     }
+
     public function addRSignUp($data){
     	return curl_request(API_PATH.'m=events&a=addRSignUp',$data);
+    }
+
+    
+    public function getEventsInfoById($data){
+    	return curl_request(API_PATH.'m=events&a=getEventsInfoById',$data);
     }
 }
